537fd909fd59ff027b088b464996fcf0b29a3b08
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2022-04-12 19:27:52 +0530

    Time: 2 ms (74.45%), Space: 40.8 MB (54.38%) - LeetHub

--- 507-perfect-number/507-perfect-number.java
+++ 507-perfect-number/507-perfect-number.java
@@ -1,10 +1,11 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
         int count=(num==1)?0:1;
-        int N=(int)Math.sqrt(num);
-        for(int i=2;i<=N;i++){
-            if(num%i==0 && i==(num/i)) count+=i;
-            else if(num%i==0) count+=(i+(num/i));
+        for(int i=2;i*i<=num;i++){
+            if(num%i==0){
+                if(i==(num/i)) count+=i;
+                else count+=(i+(num/i));
+            }
         }
         return (num==count)?true:false;
     }
