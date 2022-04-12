676db4cdd45228fa7d1b3d27b93068afd0ae3a32
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2022-04-12 20:30:35 +0530

    Time: 3 ms (30.06%), Space: 41.7 MB (33.56%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,14 +1,12 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x==0 || x==1) return x;
-        long s=0,e=x,mid=0;
-        while(s<=e){
-            mid=(s+e)/2;
-            if((x/mid)<mid) e=mid-1;
-            else if((x/mid)>mid) s=mid+1;
-            else return (int)mid;
-        }
-        return (int)e;
+        return (int)sqrt(1,x,x);
+    }
+    public long sqrt(long a,long b,int N){
+        long mid=(a+b)/2;
+        if(N==(mid*mid)||(a+1)==b) return mid;
+        if(N<(mid*mid)) return sqrt(a,mid,N);
+        return sqrt(mid,b,N);
     }
     
 }
