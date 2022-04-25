59842cb2da1d7585d7f1d58b9bef63d2f40e6ca8
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2022-04-25 16:20:06 +0530

    Time: 9 ms (85.78%), Space: 41.3 MB (95.93%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0) return false;
+        int res=0,temp=x;
+        while(temp>0){
+            res=(res*10)+(temp%10);
+            temp/=10;
+        }
+        return x==res;
+    }
+}
