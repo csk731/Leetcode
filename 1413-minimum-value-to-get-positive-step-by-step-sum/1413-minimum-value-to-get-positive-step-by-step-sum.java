535a248f7d6d1457db16cab321eb5b225f726ece
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2022-04-25 13:41:00 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (88.24%) - LeetHub

--- 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
+++ 1413-minimum-value-to-get-positive-step-by-step-sum/1413-minimum-value-to-get-positive-step-by-step-sum.java
@@ -1,10 +1,11 @@
 class Solution {
     public int minStartValue(int[] nums) {
-        int min=0,carry=0;
+        int min=nums[0],carry=0;
         for(int i=0;i<nums.length;i++){
             carry+=nums[i];
             min=Math.min(min,carry);
         }
-        return 1-min;
+        min*=(-1);
+        return (min>=0)?min+1:1;
     }
 }
