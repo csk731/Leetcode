96d715334a8f12be92b327d4ba08009919b9bf2e
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2022-04-25 19:37:57 +0530

    Time: 13 ms (24.62%), Space: 46.5 MB (37.36%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,40 +1,17 @@
 class Solution {
     public int romanToInt(String s) {
         int res=0,len=s.length();
+        HashMap<Character,Integer> hm=new HashMap<Character,Integer>();
+        hm.put('I',1);
+        hm.put('V',5);
+        hm.put('X',10);
+        hm.put('L',50);
+        hm.put('C',100);
+        hm.put('D',500);
+        hm.put('M',1000);
         for(int i=0;i<len;i++){
-            char ch=s.charAt(i);
-            if(i<len-1 && ch=='I' && s.charAt(i+1)=='V') res+=4;
-            else if(i<len-1 && ch=='I' && s.charAt(i+1)=='X') res+=9;
-            else if(i<len-1 && ch=='X' && s.charAt(i+1)=='L') res+=40;
-            else if(i<len-1 && ch=='X' && s.charAt(i+1)=='C') res+=90;
-            else if(i<len-1 && ch=='C' && s.charAt(i+1)=='D') res+=400;
-            else if(i<len-1 && ch=='C' && s.charAt(i+1)=='M') res+=900;
-            else{
-                switch(ch){
-                case('M'):
-                    res+=1000;
-                    continue;
-                case('D'):
-                    res+=500;
-                    continue;
-                case('C'):
-                    res+=100;
-                    continue;
-                case('L'):
-                    res+=50;
-                    continue;
-                case('X'):
-                    res+=10;
-                    continue;
-                case('V'):
-                    res+=5;
-                    continue;
-                case('I'):
-                    res+=1;
-                    continue;
-                }
-            }
-            i++;
+            if(i<len-1 && (hm.get(s.charAt(i))<hm.get(s.charAt(i+1)))) res+=((-1*hm.get(s.charAt(i++))+hm.get(s.charAt(i))));
+            else res+=hm.get(s.charAt(i));
         }
         return res;
     }
