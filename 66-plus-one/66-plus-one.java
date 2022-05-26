decc15a8e5df363a250e74d1b496665edad8d936
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2022-05-26 16:14:40 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (34.04%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -2,10 +2,11 @@ class Solution {
     public int[] plusOne(int[] A) {
         int carry=1,N=A.length;
         for(int i=N-1;i>=0;i--){
-            int temp=A[i]+carry;
-            A[i]=temp%10;
-            carry=temp/10;
-            if(carry==0) return A;
+            if(A[i]<9){
+                A[i]++;
+                return A;
+            }
+            A[i]=0;
         }
         int res[]=new int[N+1];
         res[0]=1;
