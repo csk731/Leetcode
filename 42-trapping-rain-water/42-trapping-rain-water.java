5c6e63d5c17afe1a47f3dfcb4a3f170489552e00
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2022-05-26 15:56:26 +0530

    Time: 1 ms (94.50%), Space: 49.4 MB (5.38%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -1,16 +1,18 @@
 class Solution {
     public int trap(int[] A) {
-        int N=A.length,volume=0,leftmax=A[0];
-        Stack<Integer> stk=new Stack<Integer>();
-        for(int i=N-1;i>1;i--){
-            if(stk.empty()) stk.push(A[i]);
-            else if(A[i]>stk.peek()) stk.push(A[i]);
-            else stk.push(stk.peek());
-        }
-        for(int i=1;i<N-1;i++){
-            int temp=Math.min(leftmax,stk.pop())-A[i];
-            if(temp>0) volume+=temp;
-            if(A[i]>leftmax) leftmax=A[i];
+        int N=A.length,volume=0,leftmax=0,rightmax=0;
+        int left=0,right=N-1;
+        while(left<=right){
+            if(A[left]>A[right]){
+                if(A[right]>=rightmax) rightmax=A[right];
+                else volume+=(rightmax-A[right]); 
+                right--;
+            }
+            else{
+                if(A[left]>=leftmax) leftmax=A[left];
+                else volume+=(leftmax-A[left]); 
+                left++;
+            }
         }
         return volume;
     }
