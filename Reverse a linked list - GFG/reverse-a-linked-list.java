5f9d1ba41117ca710708c6e00f4713ce609ae410
Chaitanya Swaroop Kumar Allu <58213944+chaitanya-allu@users.noreply.github.com>
2023-03-11 13:49:18 +0530

    Added solution - LeetHub

---
+++ Reverse a linked list - GFG/reverse-a-linked-list.java
@@ -0,0 +1,83 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.io.*;
+
+class Node{
+    int data;
+    Node next;
+    
+    Node(int x){
+        data = x;
+        next = null;
+    }
+    
+}
+class GFG{
+	static void printList(Node node) 
+	{ 
+		while (node != null) 
+		{ 
+			System.out.print(node.data + " "); 
+			node = node.next; 
+		} 
+		System.out.println(); 
+	}
+    public static void main(String args[]) throws IOException { 
+        
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t > 0){
+        
+        	int n = sc.nextInt();
+        
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+        
+            for(int i=0; i<n-1; i++)
+            {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+        
+            Solution ob = new Solution();
+            head = ob.reverseList(head);
+            printList(head); 
+            t--;
+        }
+    } 
+} 
+   
+// } Driver Code Ends
+
+
+//function Template for Java
+
+/* linked list node class:
+
+class Node {
+    int data;
+    Node next;
+    Node(int value) {
+        this.value = value;
+    }
+}
+
+*/
+
+class Solution
+{
+    //Function to reverse a linked list.
+    Node reverseList(Node head)
+    {
+        if(head==null || head.next==null) return head;
+        Node pointer1=null, pointer2=head, pointer3=head.next;
+        while(pointer2!=null){
+            pointer2.next=pointer1;
+            pointer1=pointer2;
+            pointer2=pointer3;
+            if(pointer3!=null) pointer3=pointer3.next;
+        }
+        return pointer1;
+    }
+}
