65317a3843cb380d27948ca15841c25846a3ab0d
Chaitanya Swaroop Kumar Allu <58213944+csk731@users.noreply.github.com>
2023-09-27 14:31:03 -0500

    Time: 248 ms (30.84%), Space: 50.8 MB (52.78%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,48 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-
-        // Sort the array
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicate elements for i
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found a triplet with zero sum
-                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Skip duplicate elements for j
-                    while (j < k && nums[j] == nums[j + 1]) {
-                        j++;
-                    }
-
-                    // Skip duplicate elements for k
-                    while (j < k && nums[k] == nums[k - 1]) {
-                        k--;
-                    }
-
-                    // Move the pointers
-                    j++;
-                    k--;
-                } else if (sum < 0) {
-                    // Sum is less than zero, increment j to increase the sum
-                    j++;
-                } else {
-                    // Sum is greater than zero, decrement k to decrease the sum
-                    k--;
+        HashSet<List<Integer>> set=new HashSet<List<Integer>>();
+        int N=nums.length;
+        for(int i=0;i<N-2;i++){
+            int target=-1*nums[i], s=i+1, e=N-1;
+            while(s<e){
+                int sum=nums[s]+nums[e];
+                if(sum<target) s++;
+                else if(sum==target){
+                    set.add(new ArrayList<Integer>(Arrays.asList(nums[i],nums[s],nums[e])));
+                    s++;
+                    e--;
                 }
+                else e--;
             }
         }
-        return ans;
+        List<List<Integer>> list=new ArrayList<List<Integer>>();
+        list.addAll(set);
+        return list;
     }
 }
