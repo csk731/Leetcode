59a6e411d13f6ffd8687b15c0e60ce321c8555f7
Chaitanya Swaroop Kumar Allu <58213944+csk731@users.noreply.github.com>
2023-10-22 17:37:21 -0500

    Time: 40 ms (28.4%), Space: 55.1 MB (62.37%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,37 +1,40 @@
 class Solution {
-    public void sort(int[] nums, int i, int j){
-        if(i==j) return;
-        int mid=(i+j)/2;
-        sort(nums,i,mid);
-        sort(nums,mid+1,j);
-        merge(nums, i, mid, j);
-    }
-    public void merge(int nums[], int i, int mid, int j){
-        int m=mid-i+1, n=j-mid;
-        int arr1[]=new int[m];
-        int arr2[]=new int[n];
-
-        for(int x=0;x<m;x++)
-            arr1[x]=nums[i+x];
-
-        for(int y=0;y<n;y++)
-            arr2[y]=nums[mid+1+y];
-
-        int s=0, e=0, p=i;
-        while(s<m && e<n){
-            if(arr1[s]<arr2[e]) nums[p++]=arr1[s++];
-            else nums[p++]=arr2[e++];
-        }
-        while(s<m){
-            nums[p++]=arr1[s++];
-        }
-        while(e<n){
-            nums[p++]=arr2[e++];
+    
+    // Max Heap Approach
+    
+    public void downHeapify(int i, int[] arr, int ptr){
+        int idx1=(2*i)+1, idx2=(2*i)+2;
+        int N=arr.length;
+        if(idx1>ptr) return;
+        int max=i;
+        if(idx1<=ptr && arr[idx1]>arr[max]) max=idx1;
+        if(idx2<=ptr && arr[idx2]>arr[max]) max=idx2;
+        if(max!=i){
+            int temp=arr[max];
+            arr[max]=arr[i];
+            arr[i]=temp;
+            downHeapify(max,arr,ptr);
         }
     }
-    public int[] sortArray(int[] nums) {
-        int N=nums.length;
-        sort(nums,0,N-1);
-        return nums;
+    public void buildHeap(int[] arr){
+        int N=arr.length;
+        int idx=N-1;
+        for(int i=idx;i>=0;i--){
+            downHeapify(i,arr,N-1);
+        }
+    }
+    public int[] sortArray(int[] arr) {
+        int N=arr.length;
+        buildHeap(arr);
+        int ptr=N-1;
+        while(N!=0){
+            int temp=arr[0];
+            arr[0]=arr[ptr];
+            arr[ptr]=temp;
+            ptr--;
+            downHeapify(0,arr,ptr);
+            N--;
+        }
+        return arr;
     }
 }
