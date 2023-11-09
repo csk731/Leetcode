2600e84d35144bc86d6b3fa3c06c226eff32c3e9
Chaitanya Swaroop Kumar Allu <58213944+csk731@users.noreply.github.com>
2023-11-09 16:00:54 -0600

    Time: 12 ms (26.3%), Space: 43.8 MB (86.06%) - LeetHub

--- 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.java
+++ 1885-count-number-of-homogenous-substrings/1885-count-number-of-homogenous-substrings.java
@@ -3,11 +3,8 @@ class Solution {
         int N=s.length(), MOD=1000000007;
         long count=0, ans=0;
         for(int i=0;i<N;i++){
-            char curr=s.charAt(i);
-            char nxt='$';
-            if(i<N-1) nxt=s.charAt(i+1);
             count++;
-            if(curr!=nxt){
+            if(i==N-1 || s.charAt(i) != s.charAt(i+1)){
                 ans=(ans+(((count%MOD)*((count+1)%MOD))%MOD)/2)%MOD;
                 count=0;
             }
